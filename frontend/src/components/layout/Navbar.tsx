8f66787f08c0163d4d4ecc3e9f8928f82a9d307f
egrjati <wahyuenggarj@gmail.com>
2026-03-08 17:39:55 +0700

    create navbar and add some icon

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Search, ShoppingBag, CircleUserRound } from "lucide-react";
+
 export default function Navbar() {
   return (
     <section className="w-full flex flex-col overflow-x-hidden">
@@ -8,33 +10,31 @@ export default function Navbar() {
         </p>
       </div>
       {/* top 2 */}
-      <div className="bg-[#278236] w-full h-16 flex items-center justify-between px-4">
+      <div className="bg-[#278236] w-full h-16 flex items-center justify-between px-7">
         <h1 className="text-white text-2xl font-bold font-montserrat ml-4">
           Holdme
         </h1>
         {/* Menu Tengah */}
         <div>
           <ul className="flex gap-5 font-inter text-sm text-white">
-            <li>
-              Home
-            </li>
-            <li>
-              Product
-            </li>
-            <li>
-              Blog
-            </li>
-            <li>
-              About us
-            </li>
+            <li>Home</li>
+            <li>Product</li>
+            <li>Blog</li>
+            <li>About us</li>
           </ul>
         </div>
 
         {/* End */}
-        <ul>
-          <li className="text-white text-lg font-medium hover:text-gray-300 transition-colors duration-300">
-            Contact
-          </li>
+        <ul className="flex gap-2">
+          <button className="p-2" aria-label="CircleUserRound">
+            <CircleUserRound color="white" className="w-6 h-6 text-gray-700" />
+          </button>
+          <button className="p-2" aria-label="Search">
+            <Search color="white" className="w-6 h-6 text-gray-700" />
+          </button>
+          <button className="p-2" aria-label="ShoppingBag">
+            <ShoppingBag color="white" className="w-6 h-6 text-gray-700" />
+          </button>
         </ul>
       </div>
     </section>
