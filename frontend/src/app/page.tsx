72a7357da6aab423ba34951324cb9d5c64549aad
egrjati <wahyuenggarj@gmail.com>
2026-03-16 10:42:56 +0700

    update style page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,29 @@
 export default function Home() {
   return (
-    <section className="w-full min-h-screen flex flex-col bg-[#f6f0da] overflow-x-hidden">
-      <header className="w-full md:h-[475px] bg-[#b48453] mt-[0.5px]">
-        <img src="/asset/img/Hero Section.svg" alt="Hero Banner" className="w-full h-full object-cover" />
+    <section className="w-full min-h-screen flex flex-col bg-white overflow-x-hidden">
+      <header className="w-full md:h-[475px]">
+        <img
+          src="/asset/img/Hero Section.svg"
+          alt="Hero Banner"
+          className="w-full h-full object-cover"
+        />
       </header>
 
-      <main className="w-full mt-28">
-        <h1 className="lg:text-5xl font-bold text-black text-center font-sora">
-          No.1 K-Body Care
-        </h1>
-        <p className="font-poppins text-center text-base text-black mt-5">
-          Knock is a leading Korean body care brand that brings K-beauty
-          innovation to body care.
-        </p>
+      <main className="w-full mt-28 relative ml-6">
+        {/* garis kiri */}
+        <div className="absolute left-0 top-0 h-screen bg-black w-0.5"></div>
 
-        {/* 2 kotak berpotongan */}
-        <div className="grid grid-cols-2 h-96 mt-28">
-          <div className="bg-[#e7cc6f]" />
-          <div className="bg-sky-400" />
+        {/* judul */}
+        <div className="w-full flex flex-col items-center">
+          <h1 className="lg:text-4xl font-bold text-[#222520] text-center font-sora">
+            The Articles
+          </h1>
+          <p className="font-poppins max-w-xl text-center text-base text-[#222520] mt-5">
+            Kumpulan artikel rilisan terbatas yang dirancang khusus buat nemenin
+            setiap langkah dan cerita lo di jalanan.
+          </p>
         </div>
       </main>
-
 
       <div className=" mt-28 mb-96"></div>
     </section>
