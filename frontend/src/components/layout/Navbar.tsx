c745177c34eb90bb2ed314c220c2c57df75a2ebe
egrjati <wahyuenggarj@gmail.com>
2026-04-01 14:02:54 +0700

    update navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -5,24 +5,24 @@ export default function Navbar() {
     <section className="w-full flex flex-col overflow-x-hidden">
       {/* top 1 */}
       <div className="w-full bg-[#363636] h-10 items-center justify-center flex gap-4 border-b border-gray-300">
-        <p className="text-white text-center font-bold text-[10px] font-poppins tracking-widest p-2">
+        <p className="text-white text-center font-semibold text-[10px] font-poppins tracking-[0.18em] p-2">
           ENJOY 85% OFF + FREE SHIPPING
         </p>
       </div>
       {/* top 2 */}
       <div className="bg-white w-full px-6 md:px-10 py-4 border-b border-gray-200">
         <div className="grid grid-cols-3 items-center">
-          <div className="flex items-center text-[10px] font-inter font-medium text-[#6a6a6a]">
+          <div className="flex items-center mt-1 text-[10px] font-inter font-semibold text-[#6a6a6a]">
             <button
               aria-label="Change language"
-              className="flex items-center gap-1 uppercase tracking-[0.24em]"
+              className="flex items-center gap-1 uppercase tracking-[0.18em]"
             >
               <span>Indonesia</span>
               <ChevronDown className="h-4 w-4" strokeWidth={1.8} />
             </button>
           </div>
 
-          <h1 className="text-black text-lg md:text-3xl font-semibold font-poppins text-center uppercase tracking-[0.12em]">
+          <h1 className="text-black text-xl md:text-2xl font-semibold font-poppins text-center uppercase tracking-[0.12em]">
             Holdme
           </h1>
 
@@ -39,8 +39,9 @@ export default function Navbar() {
           </div>
         </div>
 
+        {/* menu */}
         <div className="mt-5 flex justify-center">
-          <ul className="flex flex-wrap items-center justify-center gap-5 md:gap-9 font-poppins text-sm text-black uppercase tracking-[0.18em]">
+          <ul className="flex flex-wrap font-semibold items-center justify-center gap-5 md:gap-9 font-poppins text-sm text-[#6a6a6a] uppercase tracking-[0.18em]">
             <li>New</li>
             <li>Product</li>
             <li>Blog</li>
